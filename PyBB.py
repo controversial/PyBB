421e6f53d0fbefbc7eb5e886adff7c71e1f76386
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-04-18 20:51:50 -0400

    User and Forum now inherit from a common base

--- PyBB.py
+++ PyBB.py
@@ -18,6 +18,7 @@ Also tries to implement 'smart' methods for returning data:
 """
 
 import datetime
+import inspect
 from io import BytesIO
 import json
 import os
@@ -33,73 +34,105 @@ except ImportError:
 import requests
 
 
-def process_data(data):
-    '''Powers magical __getattr__ methods. Implements smart
-       functions for returning from a dictionary, specifically
-       converting dates and times to datetime objects.'''
+# ------ HELPER CLASSES ------ #
 
-    # Attempt to return a datetime object for a 13-digit timestamp
-    if str(data).isdigit() and len(str(data)) == 13:
-        return datetime.datetime.fromtimestamp(int(data) / 1000)
-    # Attempt to return a datetime object for an ISO 8601-formatted date
-    try:
-        return datetime.datetime.strptime(data, "%Y-%m-%dT%H:%M:%S.%fZ")
-    except (ValueError, TypeError):
+class AttrDict (object):
+    """ A pseudo-dict class that allows accessing of items either by item
+        access or by attribute access """
+    def __init__(self, data):
+        self.data = data
+
+    def __getattr__(self, key):
+        return self.data[key]
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+
+class _ForumObjectBase (object):
+    """ Base class inherited by all forum-related classes """
+    def __init__(self, *args, **kwargs):
+        self._name = "data"
+        self.data = {}
+        self.aliases = {}  # __getattr__ looks at this
+        self._setup(*args, **kwargs)
+
+    def _setup(self):
         pass
-    # Return the raw value if it can't be converted
-    return data
 
-
-
-class Forum:
-    def __init__(self, url):
-        self.url = url
-        self.head = requests.head(url).headers
-        if self.head.get("X-Powered-By") != "NodeBB":
-            raise ValueError('That\'s not a NodeBB forum')
-        # URL for the API page about the forum
-        self.endpoint = urljoin(self.url, 'api/')
-        self.api_url = self.endpoint
-        # Requests object for the API call
-        self.req = requests.get(self.api_url)
-        # Dict representing API response
-        self.data = json.loads(self.req.text)
-        # Forum config
-        self.configurl = urljoin(self.endpoint, 'config')
-        self.configreq = requests.get(self.configurl)
-        self.config = json.loads(self.configreq.text)
-        # Forum title
-        self.title = self.config['siteTitle']
-
-    def dump_data(self, path='.'):
-        '''Dump forum data to a JSON file'''
-        path = os.path.join(path, 'forum' + '.json')
-        with open(path, 'w') as f:
+    def dump_data(self, path="."):
+        """Dump data to a JSON file"""
+        path = os.path.join(path, self._name + ".json")
+        with open(path, "w") as f:
             f.write(json.dumps(self.data, indent=2, sort_keys=True))
 
     def __getattr__(self, key):
-        '''Magic method that will try to return data directly
-        from the API if no wrapper has been written.'''
+        """ Powers magical __getattr__ methods. Implements smart
+            functions for returning from a dictionary, specifically
+            converting dates and times to datetime objects. """
+        # Handle data if key is directly in data
         if key in self.data:
             out = self.data[key]
-        elif key in self.config:
-            out = self.config[key]
-        else:
-            out = None
-        return process_data(out)
+            # Attempt to return a datetime for a 13-digit timestamp
+            if str(out).isdigit() and len(str(out)) == 13:
+                return datetime.datetime.fromtimestamp(int(out) / 1000)
 
-    # Methods for the creation of other related types
+            # Attempt to return a datetime for an ISO 8601-formatted date
+            try:
+                return datetime.datetime.strptime(out, "%Y-%m-%dT%H:%M:%S.%fZ")
+            except (ValueError, TypeError):
+                pass
+
+            # Return the raw value if it can't be converted
+            return out
+        # Handle aliases to properties
+        elif key in self.aliases:
+            return self.__getattr__(self.aliases[key])
+        # Throw an error if key does not exist
+        else:
+            raise AttributeError(self.__class__.__name__ +
+                                 " instance has no attribute '" + key + "'")
+
+# ------ MAIN CLASSES ------ #
+
+
+class Forum(_ForumObjectBase):
+    """ Represents a NodeBB forum. Forums are composed of Users and Topics """
+    def _setup(self, url):
+        # Assert that the URL goes to a NodeBB forum
+        if requests.head(url).headers.get("X-Powered-By") != "NodeBB":
+            raise ValueError("That\'s not a NodeBB forum")
+
+        # Store some basic forum info upon instantination
+        self.url = url
+        self.endpoint = urljoin(self.url, "api/")   # URL for the API page
+        self.api_url = self.endpoint                # ^ Alias to that ^
+        self._req = requests.get(self.api_url)      # Request for the API call
+        self.basedata = json.loads(self._req.text)  # API response as a dict
+
+        self.cfurl = urljoin(self.endpoint, "config")  # |\
+        self.configreq = requests.get(self.cfurl)  # |- Forum config
+        self.config = json.loads(self.configreq.text)  # |/
+
+        self.aliases = {"title": "siteTitle"}
+
+        self.data = dict(                  # Combination of data from:
+            list(self.basedata.items()) +  # Front page
+            list(self.config.items())      # Forum config
+        )
+
+    # -- Methods for the creating subordinate types -- #
     def User(self, username):
-        ''' Allows creation of Users by Forum('url').User('username') '''
+        """ Allows creation of Users by Forum("url").User("username") """
         return User(self, username)
 
 
-class User:
+class User(_ForumObjectBase):
     def __init__(self, forum, username):
         self.forum = forum
         self.username = username
         # URL for the API page about the user
-        self.api_url = urljoin(self.forum.endpoint, 'user/' + username)
+        self.api_url = urljoin(self.forum.endpoint, "user/" + username)
         # Requests object for the API call
         self.req = requests.get(self.api_url)
         # Dict representing API response
@@ -107,7 +140,7 @@ class User:
 
     @property
     def image(self):
-        '''Returns a PIL image for the user's profile image'''
+        """Returns a PIL image for the user"s profile image"""
         imgurl = urljoin(self.forum.endpoint, self.picture)
         if not hasPIL:
             return imgurl
@@ -116,27 +149,13 @@ class User:
             file = BytesIO(imgdata)
             return Image.open(file)
 
-    def dump_data(self, path='.'):
-        '''Dump user data to a JSON file'''
-        path = os.path.join(path, self.username + '.json')
-        with open(path, 'w') as f:
-            f.write(json.dumps(self.data, indent=2, sort_keys=True))
-
-    def __getattr__(self, key):
-        '''Magic method that will try to return data directly
-        from the API if no wrapper has been written.'''
-        if key in self.data:
-            out = self.data[key]
-        else:
-            out = None
-        return process_data(out)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Get object for omz-software forums
-    forum = Forum('https://forum.omz-software.com/')
-    # Print forum title
-    print(forum.title)
+    forum = Forum("https://forum.omz-software.com/")
+    # Print some random info about the forum
+    print("Forum title: " + forum.title)
+    print("Version: " + forum.version)
+    print("Recent topic: " + forum.topics[0]["title"])
     # Get a forum user and show their profile picture
-    u = forum.User('Webmaster4o')
+    u = forum.User("Webmaster4o")
     u.image.show()
